201a352dc1d2607ba7d34ce89f8056a622bad219
Bluelos1 <s25946@pjwstk.edu.pl>
2022-06-02 23:10:42 +0200

    header+main/html+css

--- Projekt/Catering_dietetyczny.php
+++ Projekt/Catering_dietetyczny.php
@@ -2,11 +2,33 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible"content="IE-edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Pudełka</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <section id="header">
-    <a href="#"><img src="img/logo"></a>
+    <a href="Catering_dietetyczny.php"><img src="img/logo.png" class="logo" id="logo" alt=""></a>
+    <div>
+        <ul id="bar">
+            <li><a class="active" href="Catering_dietetyczny.php">Home</a></li>
+            <li><a href="Diet.php">Diety</a></li>
+            <li><a href="Menu.php">Menu</a></li>
+            <li><a href="cennik.php">Cennik</a></li>
+            <li><a href="about.php">O Nas</a></li>
+            <li><a href="contact.php">Kontakt</a></li>
+            <li><a href="cart.php"><i class="far fa-shopping-cart"></i></a></li>
+        </ul>
+    </div>
+</section>
+<section id="hero">
+    <h2>Diet Box</h2>
+    <h1> Najlepszy i najtańszy katering</h1>
+    <h4>Zamów już teraz najzdrowszy katering dietetyczny w twojej okolicy sprawdź już poniżej naszą oferte diet </h4>
+    <p>Oszczędź już 20%! na długo terminowym zakupie</p>
+    <button>Zamów</button>
 </section>
 </body>
 </html>
